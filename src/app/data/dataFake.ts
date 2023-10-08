0cd23ef83af064d4eddb37e32daf6b29e1f4b764
Gilvan Junior <gsjunior.14@gmail.com>
2023-10-08 10:27:29 -0300

    conteúdos criados e acessando os cards

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Novo Homem de Ferro anunciado",
+        "description":"Estudios Marvel anuncia novo filme do Homem de Ferro já para o ano de 2024.",
+        "photoCover":"https://images.thedirect.com/media/article_full/iron-man-tony.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Dragon Ball Super - 2023",
+        "description":"Dragon Ball Super retornará em 2023 com episódios inéditos",
+        "photoCover":"https://i0.wp.com/www.oinformantepop.com.br/wp-content/uploads/2017/01/dragon-ball_MeZDDgk.jpg?fit=1191%2C670&ssl=1"
+    },
+    {
+        "id":"3",
+        "title":"The Big Bang Theory",
+        "description":"Décima segunda temporada de 'The Big Bang Theory' será a última",
+        "photoCover":"https://imgsapp2.correiobraziliense.com.br/app/noticia_127983242361/2018/08/22/701342/20180822162433178378u.jpg"
+    },
+    {
+        "id":"4",
+        "title":"The Walking Dead",
+        "description":"The Walking Dead - A Temporada Final Continua",
+        "photoCover":"https://www.universosagas.com.br/wp-content/uploads/2022/02/The-Walking-Dead-11a-temporada.webp"
+    }
+]
